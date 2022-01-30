794f66410a1065a9d1baa9fa5e9e13082344ae62
Tuyentran2k1213 <tuyentran2k1213@gmail.com>
2022-01-30 21:53:59 +0700

    done header

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,1 +1,25 @@
-import './heading';
+const lightDarkMode = document.querySelector('.theme');
+const mode = document.querySelector('html');
+
+//script for light and dark mode
+var isLight = true;
+lightDarkMode.onclick = function(){
+    lightDarkMode.classList.toggle('sun', isLight);
+    isLight = !isLight;
+    if(isLight){
+        mode.setAttribute('data-theme', 'light');
+    } else{
+        mode.setAttribute('data-theme', 'dark');
+    }
+    
+}
+
+var Top = document.querySelector('.toTop');
+var head = document.querySelector('header');
+var sticky = head.offsetTop;
+window.onscroll = function() {showingHeader()};
+function showingHeader() {
+    var isScroll = window.pageYOffset > sticky ? true : false;
+    head.classList.toggle('scroll', isScroll);
+    Top.classList.toggle('onTop', isScroll);
+}
